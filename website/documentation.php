c78e74921af33c547ed34cdcd3465b658978eb41
Jon Jensen <jon@endpoint.com>
2013-09-13 14:06:29 +0200

    Note video for YAPC::EU 2011 talk

--- website/documentation.php
+++ website/documentation.php
@@ -8,7 +8,7 @@
 <div class="mixcase">
 
 <ul>
-<li><a href="camps-presentation-20110817/">Camps: better web development environments</a> - slides from Jon Jensen's presentation about camps at <a href="http://yapceurope.lv/ye2011/talk/3629">YAPC::EU</a> 2011</li>
+<li><a href="camps-presentation-20110817/">Camps: better web development environments</a> - slides and video from Jon Jensen's presentation about camps at <a href="http://yapceurope.lv/ye2011/talk/3629">YAPC::EU</a> 2011</li>
 <li><a href="camps-presentation-20080829/">Camps: Better Development Environments</a> - slides from a presentation &amp; demonstration Jon Jensen gave about camps at <a href="http://utos.org/">UTOSC</a> 2008</li>
 <li><a href="http://www.spreecamps.com/camps">SpreeCamps.com DevCamps documentation</a> - tutorial and FAQ specific to the SpreeCamps environment, but of general use as well</li>
 </ul>

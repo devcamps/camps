00950456c6587383f3451cc46d057180ff932fcd
Jon Jensen <jon@endpoint.com>
2015-08-26 20:48:18 -0400

    Remove locally hosted Git repository & emphasize GitHub

--- website/code.php
+++ website/code.php
@@ -7,16 +7,13 @@
 
 <div class="mixcase">
 
-<p>The DevCamps system is <a href="http://www.gnu.org/licenses/gpl.html">GPL 3+</a> free software written in <a href="http://www.perl.org/">Perl 5</a> and runs on Linux and other Unix-like operating systems. The source code is maintained in a <a href="http://git-scm.com/">Git</a> repository, <a href="/cgi-bin/gitweb">browsable via gitweb</a>. To clone a copy:</p>
+<p>The DevCamps system is free software (<a href="http://www.gnu.org/licenses/gpl.html">GPL</a>) written in <a href="https://www.perl.org/">Perl 5</a> and runs on Linux and other Unix-like operating systems. The source code is maintained in a <a href="https://github.com/devcamps/camps">Git repository hosted at GitHub user “devcamps”</a>.
 
-<pre>git clone http://git.devcamps.org/camps.git
+<p>If you have any problems to report or improvements to suggest, open a GitHub issue or write to us on the <a href="https://mail.endcrypt.com/mailman/listinfo/camps-users">camps-users mailing list</a>. We welcome GitHub pull requests and patches as well.</p>
 
-</pre>
+<p>(References to our previous Git repository at http://git.devcamps.org/camps.git should be changed to <a href="https://github.com/devcamps/camps.git">https://github.com/devcamps/camps.git</a>.)</p>
 
-<p>We have a <a href="http://github.com/devcamps/camps/tree/master">GitHub mirror</a> of the repository as well. If you have any changes to submit, please fork the repository, push your changes, and tell us about them on the <a href="https://mail.endcrypt.com/mailman/listinfo/camps-users">camps-users mailing list</a>.</p>
-
-<p>You can see an overview report about the code on our<br />
-<a href="https://www.ohloh.net/p/devcamps">DevCamps Ohloh project page</a>.</p>
+<p>You can see an overview report about the code on our <a href="https://www.openhub.net/p/devcamps">DevCamps Open Hub project page</a>.</p>
 
 </div>
 

756bf3ccd632a32368b43864baade396a5e81f5f
Jon Jensen <jon@endpoint.com>
2016-03-03 00:02:25 -0500

    Add links to Sam Batschelet's presentations on camps

--- website/documentation.php
+++ website/documentation.php
@@ -8,6 +8,8 @@
 <div class="mixcase">
 
 <ul>
+<li><a href="https://www.perl.dance/talks/4-space-camp---the-final-frontier">Space Camp :: The Final Frontier</a> - presentation <a href="https://www.youtube.com/watch?v=IYKs8FF8--Y">video</a> about a next-generation experiment to extend camps by Sam Batschelet (hexfusion) at the Ecommerce Innovation 2015 conference</li>
+<li>Camps Development Platform - <a href="https://www.youtube.com/watch?v=bs_yU9boMZ0">video</a> and <a href="http://www.icdevgroup.org/slides/eic2013/Camps.pdf">slides</a> from a presentation Sam Batschelet (hexfusion) gave at the Ecommerce Innovation 2013 conference</li>
 <li><a href="camps-presentation-20110817/">Camps: better web development environments</a> - slides and video from Jon Jensen's presentation about camps at <a href="http://yapceurope.lv/ye2011/talk/3629">YAPC::EU</a> 2011</li>
 <li><a href="camps-presentation-20080829/">Camps: Better Development Environments</a> - slides from a presentation &amp; demonstration Jon Jensen gave about camps at <a href="http://utos.org/">UTOSC</a> 2008</li>
 <li><a href="http://www.spreecamps.com/camps">SpreeCamps.com DevCamps documentation</a> - tutorial and FAQ specific to the SpreeCamps environment, but of general use as well</li>

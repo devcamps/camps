df78f2b283217f9e0fde4613a552653db15b92ec
Jon Jensen <jon@endpoint.com>
2021-02-11 16:49:16 +0000

    Remove Open Hub link and update a URL

--- website/code.php
+++ website/code.php
@@ -7,13 +7,11 @@
 
 <div class="mixcase">
 
-<p>The DevCamps system is free software (<a href="http://www.gnu.org/licenses/gpl.html">GPL</a>) written in <a href="https://www.perl.org/">Perl 5</a> and runs on Linux and other Unix-like operating systems. The source code is maintained in a <a href="https://github.com/devcamps/camps">Git repository hosted at GitHub user “devcamps”</a>.
+<p>The DevCamps system is free software (<a href="https://www.gnu.org/licenses/gpl.html">GPL</a>) written in <a href="https://www.perl.org/">Perl 5</a> and runs on Linux and other Unix-like operating systems. The source code is maintained in a <a href="https://github.com/devcamps/camps">Git repository hosted at GitHub user “devcamps”</a>.
 
 <p>If you have any problems to report or improvements to suggest, open a GitHub issue or write to us on the <a href="https://www.interchangecommerce.org/mailman/listinfo/camps-users">camps-users mailing list</a>. We welcome GitHub pull requests and patches as well.</p>
 
 <p>(References to our previous Git repository at http://git.devcamps.org/camps.git should be changed to <a href="https://github.com/devcamps/camps.git">https://github.com/devcamps/camps.git</a>.)</p>
-
-<p>You can see an overview report about the code on our <a href="https://www.openhub.net/p/devcamps">DevCamps Open Hub project page</a>.</p>
 
 </div>
 
